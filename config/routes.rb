7821c6e610fe30a3535b3735b4a23150b9443db0
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-20 17:19:20 +0200

    nested routes for locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'locations_controller/index'
-  get 'locations_controller/show'
-  get 'locations_controller/new'
-  get 'locations_controller/create'
-  get 'locations_controller/destroy'
-  get 'instruments', to: 'instruments#index'
-  get 'instruments/:id', to: 'instruments#show'
-  get 'instruments/new'
-  get 'instruments/create'
-  get 'instruments/update'
-  get 'instruments/edit'
+  resources :instruments do
+    resources :locations
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
