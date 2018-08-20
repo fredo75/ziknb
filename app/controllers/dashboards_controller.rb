3324dbd1f38a867078f81beda896d8ac622608cb
adrien brun <adrien93@hotmail.com>
2018-08-20 16:50:35 +0200

    I changed the name of the method of the dashboard model

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
 
-  def home
+  def dashboard_home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
 
 
-  get 'dashboards/', to: "dashboards#home"
+  get 'dashboards/', to: "dashboards#dashboard_home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
