fd826c75e1a3e8aa3960f4dda2ba03b8e9f72bf1
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-20 17:10:52 +0200

    locations model et controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'locations_controller/index'
+  get 'locations_controller/show'
+  get 'locations_controller/new'
+  get 'locations_controller/create'
+  get 'locations_controller/destroy'
   get 'instruments', to: 'instruments#index'
   get 'instruments/:id', to: 'instruments#show'
   get 'instruments/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_140052) do
+ActiveRecord::Schema.define(version: 2018_08_20_150742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2018_08_20_140052) do
     t.string "marque"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.integer "date"
+    t.integer "price"
+    t.boolean "delivered"
+    t.bigint "user_id"
+    t.bigint "instrument_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_locations_on_instrument_id"
+    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +47,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_140052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "locations", "instruments"
+  add_foreign_key "locations", "users"
 end
