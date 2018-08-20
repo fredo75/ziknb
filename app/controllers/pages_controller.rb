731c0ce8bd46a47b99907fc2752be861cdd721db
Camille Franc <Camille.franc.pro@gmail.com>
2018-08-20 18:56:22 +0200

    linking homepage with sign in / sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,5 @@
 class PagesController < ApplicationController
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   skip_before_action :authenticate_user!, only: :home
-
->>>>>>> master
-=======
-  skip_before_action :authenticate_user!, only: :home
->>>>>>> master
   def home
   end
 end
