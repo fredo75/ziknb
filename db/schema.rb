6b4b4118f6141ff30af96b33ca5de22768acd49d
Camille Franc <Camille.franc.pro@gmail.com>
2018-08-21 19:45:29 +0200

    Ne Pas merger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_131109) do
+ActiveRecord::Schema.define(version: 2018_08_21_150716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_08_21_131109) do
     t.bigint "instrument_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["instrument_id"], name: "index_locations_on_instrument_id"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
