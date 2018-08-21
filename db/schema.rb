c1985f494d8c9b750d4df6ad2d41d6ca35c0b345
adrien brun <adrien93@hotmail.com>
2018-08-21 10:19:08 +0200

    no changes

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_162846) do
     t.string "marque"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.bigint "user_id"
     t.index ["user_id"], name: "index_instruments_on_user_id"
-
   end
 
   create_table "locations", force: :cascade do |t|
