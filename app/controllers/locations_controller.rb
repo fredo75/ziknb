fdfd16418fa9f7dbc9f7faa238e8c7996d9d49f1
Camille Franc <Camille.franc.pro@gmail.com>
2018-08-21 14:20:03 +0200

    work on location model show new create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,10 @@
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def new
@@ -11,8 +13,20 @@ class LocationsController < ApplicationController
   end
 
   def create
+    @instrument = Instrument.find(params[:instrument_id])
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to new_location_path(@location)
+    else
+      render 'index'
+    end
   end
 
   def destroy
   end
+end
+
+private
+def location_params
+  params.require(:location).permit(:date)
 end
