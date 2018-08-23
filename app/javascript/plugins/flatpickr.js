21ba3c6538ef435f67808108b6d98dbdd6bc0b93
frederic curier <frederic.curier@gmail.com>
2018-08-23 18:34:55 +0200

    fix final search bar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,3 +2,5 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 flatpickr(".datepicker", {})
+
+flatpickr(".datepicker-home", {mode: "range"})
