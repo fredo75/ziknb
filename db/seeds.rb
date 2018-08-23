2b2a05843f51ba99131cb14ac13f72aa77b1bcc4
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-23 12:32:24 +0200

    seed images & card

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ Instrument.destroy_all
 puts "create User"
 bidon = User.create(email: "bidon@gmail.com", password: "azerty")
 donbi = User.create(email: "donbi@gmail.com", password: "azerty")
+karl = User.create(email: "karl@gmail.com", password: "azerty")
 p User.all
 
 puts "create Instruments"
@@ -26,6 +27,8 @@ guitare_1 = Instrument.create(title: "guitare", description:"guitare stratocaste
 guitare_2 = Instrument.create(title: "guitare", description:"guitare Telecaster",  marque: "Fender", address: "15 villa gaudelet, paris",  user: donbi)
 guitare_3 = Instrument.create(title: "guitare folk", description:"guitare acoustique",  marque: "Takamine", address: "20 avenue de menillmontant, paris", user: donbi)
 guitare_4 = Instrument.create(title: "guitare classique", description:"guitare classique pro",  marque: "Takamine", address: "20 rue verte, paris", user: donbi)
+banjo = Instrument.create(title: "custom banjo", description: "Cripple Creek banjo", marque: "Gold Tone", address: "9 rue de charone, paris", user: bidon)
+drum = Instrument.create(title: "homemade drum", description: "Waiting for you to make everyone dance", marque: "Tama", address:"115 rue Lafayette, paris", user: karl )
 
 
 loc = Location.new(date: 1522, price: 220, delivered: true, user: bidon, instrument: guitare)
@@ -33,18 +36,22 @@ loc.save!
 
 
 
-guitare.remote_photo_url = "https://www.woodbrass.com/images/SQUARE400/woodbrass/ESP+GEV2EC2VBK+GUITARE+ELECTRIQUE.JPG"
+guitare.remote_photo_url = "https://images.unsplash.com/photo-1508186736123-44a5fcb36f9f?ixlib=rb-0.3.5&s=93a5334e00639f481ac6021361a5ba34&auto=format&fit=crop&w=668&q=80"
 guitare.save
-uekulele.remote_photo_url = "https://www.thomann.de/pics/bdb/410343/12582286_800.jpg"
+uekulele.remote_photo_url = "https://images.unsplash.com/photo-1505473286910-580cec54a3ed?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=250defea85465f001b7ec0b37f4b358e&auto=format&fit=crop&w=1550&q=80"
 uekulele.save
-guitare_1.remote_photo_url = "https://www.woodbrass.com/images/SQUARE400/woodbrass/ESP+GEV2EC2VBK+GUITARE+ELECTRIQUE.JPG"
+guitare_1.remote_photo_url = "https://images.unsplash.com/photo-1515850031371-8230418d8aed?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9ea0ed08d3536a3e6bb1962d73bb8580&auto=format&fit=crop&w=1304&q=80"
 guitare_1.save
-guitare_2.remote_photo_url = "https://www.woodbrass.com/images/SQUARE400/woodbrass/ESP+GEV2EC2VBK+GUITARE+ELECTRIQUE.JPG"
+guitare_2.remote_photo_url = "https://images.unsplash.com/photo-1519087409225-c6c6f7fd0aee?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c55ec38c8c2e7ad8f5c238acea33bf1c&auto=format&fit=crop&w=1350&q=80"
 guitare_2.save
-guitare_3.remote_photo_url = "https://www.michenaud.com/upDATA/p/1500/1165/img1431960760-eZ5lkdG/zoom.jpg"
+guitare_3.remote_photo_url = "https://images.unsplash.com/photo-1457960784918-ea479c62a2c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5c8592eda66df40f7ad15d8b4cce2e30&auto=format&fit=crop&w=750&q=80"
 guitare_3.save
-guitare_4.remote_photo_url = "https://www.woodbrass.com/images/SQUARE400/woodbrass/ESP+GEV2EC2VBK+GUITARE+ELECTRIQUE.JPG"
+guitare_4.remote_photo_url = "https://images.unsplash.com/photo-1465225314224-587cd83d322b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b30ae759059ac8e7562ad32f1c7417ee&auto=format&fit=crop&w=1350&q=80"
 guitare_4.save
+banjo.remote_photo_url = "https://images.unsplash.com/photo-1482443462550-d2c99314ab6a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d99dbfa934a0f05dce23f04ac67885f0&auto=format&fit=crop&w=1502&q=80"
+banjo.save
+drum.remote_photo_url = "https://images.unsplash.com/photo-1470784591255-6c7c80d419c8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6e957cd463132b7fc85f9ecdc45b1f2c&auto=format&fit=crop&w=1252&q=80"
+drum.save
 
 
 
