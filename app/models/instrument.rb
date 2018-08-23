4208d9983114b898fd9c98f44c27c4d82ca0536d
adrien brun <adrien93@hotmail.com>
2018-08-23 11:42:04 +0200

    changes in the instrument model and instrument form

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,4 +3,10 @@ class Instrument < ApplicationRecord
   belongs_to :user
   has_many :locations
   mount_uploader :photo, PhotoUploader
+
+CATEGORIES = ["Instruments à vent", "Instruments à corde", "percurssion", "autres categories d'instrument"]
+
+
+
+
 end
